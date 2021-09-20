d0b9d6a2e233de8830bafca5f6634315a80a44de
tcspariwesh <pariweshg@gmail.com>
2021-09-20 22:37:42 +0530

    record funda1

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -2,7 +2,24 @@
 
 namespace Csharp
 {
-    public record Person(string FirstName, string LastName);
+    //public record Person(string FirstName, string LastName);
+    public record Person
+    {
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+
+        public void Deconstruct(out string firstName, out string lastName)
+        {
+            firstName = FirstName;
+            lastName = LastName;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
