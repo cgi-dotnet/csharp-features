dca0321082426442b1c58cdf84bde38b6b69e8a9
cgidna01 <cgidna01@CGIDOAX01>
2021-09-20 22:32:37 +0530

    Add project files.

---
+++ Csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Csharp
+{
+    public record Person(string FirstName, string LastName);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("John", "Doe");
+            (var firstName, var lastName) = person;
+            Console.WriteLine($"Hello World! {lastName}");
+        }
+    }
+}
